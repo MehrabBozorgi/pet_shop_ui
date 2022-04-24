dc0ca633fd145dc17137256fef20f4c2ff66a341
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-04-24 06:34:28 +0430

    pet_shop_ui

---
+++ lib/style.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+const bigTitleStyle1 = TextStyle(
+  fontSize: 35,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+//
+const bigTitleStyle2 = TextStyle(
+  fontSize: 28,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+//
+const buttonWhiteStyle = TextStyle(
+  fontSize: 28,
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+);
+//
+const titleStyle = TextStyle(
+  fontSize: 18,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+//
+const searchStyle = TextStyle(
+  fontSize: 16,
+  color: Colors.grey,
+);
+//
+const greyStyle = TextStyle(
+  fontSize: 14,
+  color: Colors.grey,
+  fontWeight: FontWeight.bold,
+);
